c379be61932116d1a6eef06db9cae633d5b7c4f8
NgQuy612 <nguyenngocquy612@gmail.com>
2023-09-25 16:07:20 +0700

    new

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,25 +1,46 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import images from '~/assets/images';
+import Search from '../../components/Search';
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [fallback, setFallback] = useState('');
+
+    const handleError = () => {
+        setFallback(images.noImage);
+    };
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <div className={cx('logo')}></div>
-                <div className={cx('search')}>
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                    <input spellCheck={false} />
-                    <button className={cx('clear-btn')}>
-                        <FontAwesomeIcon icon={faXmark} />
-                    </button>
+                <Link to="/" className={cx('logo')}>
+                    <img
+                        src={
+                            fallback ||
+                            'https://themes.sikidodemo.com/demo-website/web22/uploads/source//logo/logo-(3).webp'
+                        }
+                        alt="logo"
+                        onError={handleError}
+                    />
+                </Link>
+                <Search />
+                <div className={cx('category')}>
+                    <Link className={cx('item-iphone')} to="/product">
+                        Iphone
+                    </Link>
+                    <Link className={cx('item-ipad')} to="/product">
+                        Ipad
+                    </Link>
+                    <Link className={cx('item-mac')} to="/product">
+                        Mac
+                    </Link>
+                    <Link className={cx('item-ipad')} to="/product">
+                        Liên Hệ
+                    </Link>
                 </div>
-                <div className={cx('action')}></div>
             </div>
         </header>
     );
---
+++ src/components/Layout/components/Search/index.js
@@ -0,0 +1,74 @@
+import { useEffect, useState, useRef } from 'react';
+import { faXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeadlessTippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 0);
+    }, []);
+
+    const handleClear = () => {
+        setSearchValue('');
+        setSearchResult([]);
+        inputRef.current.focus();
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    return (
+        <HeadlessTippy
+            interactive
+            visible={showResult && searchResult.length > 0}
+            render={(attrs) => (
+                <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper>
+                        <h4 className={cx('search-title')}>Accounts</h4>
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                        <AccountItem />
+                    </PopperWrapper>
+                </div>
+            )}
+            onClickOutside={handleHideResult}
+        >
+            <div className={cx('search')}>
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+                <input
+                    ref={inputRef}
+                    value={searchValue}
+                    placeholder="Nhập sản phẩm tìm kiếm"
+                    spellCheck={false}
+                    onChange={(e) => setSearchValue(e.target.value)}
+                    onFocus={() => setShowResult(true)}
+                />
+                {!!searchValue && (
+                    <button className={cx('clear-btn')} onClick={handleClear}>
+                        <FontAwesomeIcon icon={faXmark} />
+                    </button>
+                )}
+            </div>
+        </HeadlessTippy>
+    );
+}
+
+export default Search;
