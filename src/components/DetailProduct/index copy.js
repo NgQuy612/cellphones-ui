277345509e6046e663e1914f9176d88b186577f9
NgQuy612 <nguyenngocquy612@gmail.com>
2023-10-25 11:41:52 +0700

    Tuần 5 fix

---
+++ src/components/DetailProduct/index copy.js
@@ -0,0 +1,96 @@
+import styles from './DetailProduct.module.scss';
+import classNames from 'classnames/bind';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+const ColorButton = styled.button`
+    background-color: ${(props) => props.color};
+    margin-right: 18px;
+    width: 56px;
+    height: 56px;
+    border-radius: 100%;
+    box-shadow: inset 0 3px 4px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+`;
+//{ api } function DetailProduct({ api })
+function DetailProduct({ typeModel }) {
+    
+    const { id } = useParams();
+    useEffect(() => {
+        fetch(`http://localhost:3000/${typeModel}/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // Xử lý data
+            })
+            .catch((error) => console.error('Lỗi khi tải danh sách sản phẩm:', error));
+    }, []);
+
+    const handlePriceChange = (newPrice) => {
+        setCurrentPrice(newPrice);
+    };
+
+    const handleColorChange = (newColor, newImage) => {
+        setCurrentColor(newColor);
+        setImageURL(newImage);
+    };
+
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner-wrapper')}>
+                <div className={cx('img-product')}>
+                    <img src={imageURL} alt="image iphone" />
+                </div>
+                <div className={cx('detail-product')}>
+                    <h1 className={cx('title-product')}>{product.name}</h1>
+                    <p className={cx('ready-product')}>{product.ready ? 'Còn hàng' : 'Hết hàng'}</p>
+                    <p className={cx('price-product')}>{currentPrice}</p>
+                    <p className={cx('title-local-product')}>Dung lượng</p>
+                    <div className={cx('local-product')}>
+                        {localPrice.map((item, index) => (
+                            <button
+                                key={index}
+                                className={cx('btn-local-product')}
+                                onClick={() => handlePriceChange(`${item.price}`)}
+                            >
+                                {item.local}
+                            </button>
+                        ))}
+                    </div>
+                    <p className={cx('title-color-product')}>Màu: {currentColor} </p>
+                    <div className={cx('color-product')}>
+                        {colorUrl.map((item, index) => (
+                            <ColorButton
+                                key={index}
+                                color={item.color}
+                                onClick={() => handleColorChange(`${item.name_color}`, `${item.url}`)}
+                            ></ColorButton>
+                        ))}
+                    </div>
+                    <img
+                        className={cx('img-policy-product')}
+                        src="https://cdn.tgdd.vn/2023/10/banner/Frame-482029--1--920x230.png"
+                        alt="img-policy"
+                    />
+                    <div className={cx('polyci-product')}>
+                        <ul className={cx('list-polyci-product')}>
+                            <li className={cx('item-polyci-product')}>
+                                <a href="/">Bộ sản phẩm gồm: Hộp, Sách hướng dẫn, Cây lấy sim, Cáp Type C</a>
+                            </li>
+                            <li className={cx('item-polyci-product')}>
+                                <a href="/">Hư gì đổi nấy 12 tháng tại 3453 siêu thị trên toàn quốc</a>
+                            </li>
+                            <li className={cx('item-polyci-product')}>
+                                <a href="/">Bảo hành chính hãng 1 năm</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DetailProduct;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -54,30 +54,30 @@ function Home() {
 
                     <div className={cx('list-item-card')}>
                         <Card
-                            to="/iphone/detail-iphone"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/42/303832/s16/iphone-15-pro-blue-thumbtz-650x650.png"
-                            title="iPhone 15 Pro 512GB"
-                            price="37.590.000₫"
-                        />
-                        <Card
-                            to="/iphone/detail-iphone"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/42/153856/s16/iphone-11-white-thumbtz-650x650.png"
+                            to="/iphone/detail-iphone/1"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/42/153856/s16/iphone-11-white-thumbtz-650x650.png"
                             title="iPhone 11 64GB"
-                            price="10.190.000₫"
+                            price="11.590.000₫"
+                        />
+                        <Card
+                            to="/iphone/detail-iphone/2"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/42/299033/s16/iphone-15-pro-blue-thumbtz-650x650.png"
+                            title="iPhone 15 Pro"
+                            price="27.990.000₫"
                             hot
                         />
                         <Card
-                            to="/iphone/detail-iphone"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/42/289663/s16/iphone-14-blue-thumbtz-650x650.png"
-                            title="iPhone 14 256GB"
-                            price="22.090.000₫"
+                            to="/iphone/detail-iphone/3"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/42/245545/s16/iphone-14-plus-gold-thumbtz-650x650.png"
+                            title="iPhone 13 64GB"
+                            price="21.590.000₫"
                             hot
                         />
                         <Card
-                            to="/iphone/detail-iphone"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/42/281570/s16/iphone-15-green-thumbtz_0-650x650.png"
-                            title="iPhone 15"
-                            price="21.990.000₫"
+                            to="/iphone/detail-iphone/4"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/42/303891/s16/iphone-15-plus-blue-thumbtz-1-650x650.png"
+                            title="iPhone 15 Plus"
+                            price="22.590.000₫"
                         />
                     </div>
                 </div>
@@ -90,30 +90,30 @@ function Home() {
                     </div>
                     <div className={cx('list-item-card')}>
                         <Card
-                            to="/ipad/detail-ipad"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/522/247517/s16/ipad-gen-9-silver-label-650x650.png"
-                            title="iPad 9 WiFi 64GB"
-                            price="7.890.000₫"
-                        />
-                        <Card
-                            to="/ipad/detail-ipad"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/522/295467/s16/iPad-Pro-M2-129-5G-grey-thumb-label-650x650.png"
+                            to="/ipad/detail-ipad/1"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/522/295467/s16/iPad-Pro-M2-129-5G-sliver-thumb-label-650x650.png"
                             title="iPad Pro M2 12.9 inch WiFi Cellular 128GB"
-                            price="32.090.000₫"
-                            hot
+                            price="32.950.000₫"
                         />
                         <Card
-                            to="/ipad/detail-ipad"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/522/295467/s16/iPad-Pro-M2-129-5G-grey-thumb-label-650x650.png"
+                            to="/ipad/detail-ipad/2"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/522/295453/s16/ipad-gen-10-cellular-sliver-650x650.png"
                             title="iPad 10 WiFi + Cellular"
                             price="14.290.000₫"
                             hot
                         />
                         <Card
-                            to="/ipad/detail-ipad"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/522/274156/s16/air-5-m1-wifi-cellular-xanh-thumb-label-650x650.png"
+                            to="/ipad/detail-ipad/3"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/522/274155/s16/air-5-m1-wifi-cellular-xanh-thumb-label-650x650.png"
                             title="iPad Air 5 WiFi + Cellular"
                             price="21.590.000₫"
+                            hot
+                        />
+                        <Card
+                            to="/ipad/detail-ipad/4"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/522/295467/s16/iPad-Pro-M2-129-5G-sliver-thumb-label-650x650.png"
+                            title="iPad Pro M2 12.9 inch WiFi Cellular 128GB"
+                            price="32.090.000₫"
                         />
                     </div>
                 </div>
@@ -126,30 +126,29 @@ function Home() {
                     </div>
                     <div className={cx('list-item-card')}>
                         <Card
-                            to="/watch/detail-watch"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316008/s16/apple-watch-se-2023-vien-nhom-day-vai-trang-tb-650x650.png"
+                            to="/watch/detail-watch/1"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316015/s16/apple-watch-se-2023-vien-nhom-day-vai-xanh-den-tb-650x650.png"
                             title="Apple Watch SE 2023 GPS 40mm Sport Loop"
-                            price="6.390.000₫"
+                            price="7.190.000₫"
                         />
                         <Card
-                            to="/watch/detail-watch"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316015/s16/apple-watch-se-2023-vien-nhom-day-vai-xanh-den-tb-650x650.png"
-                            title="Apple Watch SE 2023 GPS 44mm Sport Loop"
+                            to="/watch/detail-watch/2"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316012/s16/apple-watch-se-2023-vien-nhom-day-silicone-xanh-den-tb-650x650.png"
+                            title="Apple Watch SE 2023 GPS 44mm Sport Band"
                             price="7.190.000₫"
                             hot
                         />
                         <Card
-                            to="/watch/detail-watch"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316012/s16/apple-watch-se-2023-vien-nhom-day-silicone-xanh-den-tb-650x650.png"
-                            title="Apple Watch SE 2023 GPS 44mm Sport Band"
-                            price="6.390.000₫"
-                        />
-                        <Card
-                            to="/watch/detail-watch"
+                            to="/watch/detail-watch/3"
                             url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316004/s16/apple-watch-se-lte-2023-vien-nhom-day-vai-trang-tb-1-650x650.png"
                             title="Apple Watch SE 2023 GPS + Cellular 40mm Sport Loop"
                             price="8.190.000₫"
-                            hot
+                        />
+                        <Card
+                            to="/watch/detail-watch/1"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/7077/316015/s16/apple-watch-se-2023-vien-nhom-day-vai-xanh-den-tb-650x650.png"
+                            title="Apple Watch SE 2023 GPS 40mm Sport Loop"
+                            price="7.190.000₫"
                         />
                     </div>
                 </div>
@@ -162,30 +161,30 @@ function Home() {
                     </div>
                     <div className={cx('list-item-card')}>
                         <Card
-                            to="/mac/detail-mac"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/44/231244/s16/mac-air-13-m1-xam-650x650.png"
-                            title="MacBook Air 13 inch M1 2020 8CPU/8GB/256GB/7GPU"
-                            price="19.490.000₫"
-                        />
-                        <Card
-                            to="/mac/detail-mac"
+                            to="/mac/detail-mac/1"
                             url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/44/309018/s16/mac-air-15-m2-starlight-650x650.png"
                             title="MacBook Air 15 inch M2 2023 8CPU - 10GPU"
+                            price="32.090.000₫"
+                        />
+                        <Card
+                            to="/mac/detail-mac/2"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/44/302150/s16/mac-pro-16-m2-bac-650x650.png"
+                            title="MacBook Pro 16 inch 2023 M2 Pro 12CPU - 19GPU"
                             price="32.090.000₫"
                             hot
                         />
                         <Card
-                            to="/mac/detail-mac"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/44/302150/s16/mac-pro-16-m2-bac-650x650.png"
-                            title="MacBook Pro 16 inch 2023 M2 Pro 12CPU - 19GPU"
+                            to="/mac/detail-mac/3"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/44/231244/s16/mac-air-13-m1-xam-650x650.png"
+                            title="MacBook Air 13 inch M1 2020 8CPU - 7GPU"
                             price="19.490.000₫"
                             hot
                         />
                         <Card
-                            to="/mac/detail-mac"
-                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100,s_300x300/https://cdn.tgdd.vn/Products/Images/44/231244/s16/mac-air-13-m1-xam-650x650.png"
-                            title="MacBook Air 13 inch M1 2020 8CPU/8GB/256GB/7GPU"
-                            price="19.490.000₫"
+                            to="/mac/detail-mac/1"
+                            url="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/Products/Images/44/309018/s16/mac-air-15-m2-starlight-650x650.png"
+                            title="MacBook Air 15 inch M2 2023 8CPU - 10GPU"
+                            price="32.090.000₫"
                         />
                     </div>
                 </div>
